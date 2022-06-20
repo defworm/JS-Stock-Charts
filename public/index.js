58b87d4d64737221df507a1ab88b94a80b503a2f
Crystal Korth <crystalkorth@gmail.com>
2022-06-20 14:58:58 -0500

    had to move my const response below the declaration

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 async function main() {
-    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX')
-
+    
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=15min&apikey=40c1a9bcc08148c2b1bcd400e7457014')
+    const responseText = await response.text();
+    console.log(responseText);
 }
 
 main()

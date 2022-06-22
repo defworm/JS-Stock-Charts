bf60c63f505b4adb653bee789a7396b5fe8408f6
Crystal Korth <crystalkorth@gmail.com>
2022-06-21 22:34:05 -0500

    finished chart 1

--- public/index.js
+++ public/index.js
@@ -1,3 +1,18 @@
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
 async function main() {
     
     const timeChartCanvas = document.querySelector('#time-chart');
@@ -14,24 +29,25 @@ async function main() {
     const { GME, MSFT, DIS, BNTX } = mockData; //result;
 
     const stocks = [GME, MSFT, DIS, BNTX];
+    stocks.forEach (stock => stock.values.reverse())
     new Chart(timeChartCanvas.getContext('2d'), {
         type: 'line',
         data: {
             labels:stocks[0].values.map( value => value.datetime),
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: 
-                    'rgba(255, 99, 132, 0.2)',
+            datasets: stocks.map ( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat (value.high)),
+                backgroundColor: getColor (stock.meta.symbol),
+                    
                    
                 
-                borderColor: [
-                    'rgba(255, 99, 132, 1)'
+                borderColor: getColor(stock.meta.symbol),
+                    
                     
 
-                ],
+                
                
-            }]
+            }))
         }
     });
     console.log(stocks[0].values)
